9861ac5625a11532f4d5424290be0864cf1cb16d
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2022-10-04 13:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,77 +1,75 @@
+provider "vault" {
+
+  address = "http://127.0.0.1:8200"
+
+}
+
+
+
+resource "vault_generic_secret" "example" {
+
+  path = "secret/hello"
+
+
+
+  data_json = jsonencode(
+
+    {
+
+      "foo"   = "bar",
+
+      "pizza" = "cheese"
+
+    }
+
+  )
+
+}
+
+
+
 provider "aws" {
 
+  access_key = var.access_key
 
+  secret_key = var.secret_key
 
- access_key          = "${var.access_key}"
-
-
-
- secret_key          = "${var.secret_key}"
-
-
-
- region              = "${var.region}"
-
-
+  region     = var.region
 
 }
-
-
-
-variable "access_key" {}
-
-
-
-variable "secret_key" {}
-
-
-
-variable "region" {
-
-
-
- type  = string
-
-
-
-}
-
-
-
-
-
-variable "IN_TYPE" {
-
-
-
- type    = string
-
-
-
- default = "t2.micro"
-
-
-
-}
-
-
-
-
 
 resource "aws_instance" "example" {
 
+   ami = var.ami_id
 
+   instance_type = var.IN_TYPE
 
- ami           = "ami-06fcc1f0bc2c8943f"
+   subnet_id   = "subnet-aa7543ce"
 
+}
 
+variable "access_key" {}
 
- instance_type = var.IN_TYPE
+variable "secret_key" {}
 
+variable "region" {
 
+   type  = string
 
- subnet_id   = "subnet-aa7543ce"
+}
 
+variable "IN_TYPE" {
 
+   type    = string
+
+   default = "t2.micro"
+
+}
+
+variable "ami_id" {
+
+  default = "ami-06fcc1f0bc2c8943f"
+
+  type    = string
 
 }
